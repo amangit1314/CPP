5d7a64b4e6c8a69dfb51159886abec4830c1ce72
[Aman] <[boat.realme@gmail.com]>
2020-10-14 14:30:29 +0530

    first commit

---
+++ programs/Hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int s = argc;
+    if(s> 5000){
+        cout<<"Neha\n";
+    }else{
+        cout<<"Friends";
+    }
+    cout<<"Hello Cpp\n";
+}
