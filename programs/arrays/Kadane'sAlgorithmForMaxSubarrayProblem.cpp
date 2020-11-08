9b6cb7585adc06dce3954ed691f42d0d2b251316
Aman Soni <65482419+gitaman8481@users.noreply.github.com>
2020-11-08 16:46:58 +0530

    Modification 4

---
+++ programs/arrays/Kadane'sAlgorithmForMaxSubarrayProblem.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<climits>
+//Kadane's Algorithm
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+
+    int maxSum = INT_MIN;
+    int currentSum = 0;
+
+    for(int i=0 ;i<n ;i++){
+        currentSum += a[i];
+        if(currentSum<0)
+            currentSum=0;
+
+        maxSum = max(maxSum, currentSum);
+    }
+    
+     cout<<maxSum<<endl;
+    return 0;
+}
