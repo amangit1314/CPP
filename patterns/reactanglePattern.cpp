a0baf033b73a8f9ab814673ac87a430f6f24ca76
Aman Soni <65482419+gitaman8481@users.noreply.github.com>
2021-01-05 21:04:37 +0530

    seventeenth commit

---
+++ patterns/reactanglePattern.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    cin >> row;
+    cin >> col;
+
+    for (int i = 1; i <= row; i++){
+        for (int j = 1; i <= col; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
